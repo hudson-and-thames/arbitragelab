92181c6a87bf08cc8552c105b057f58dee4efe28
PanPip <panpipp@gmail.com>
2020-11-06 19:08:27 +0100

    Add Distance Approach unit tests

---
+++ arbitragelab/__init__.py
@@ -0,0 +1,6 @@
+"""
+ArbitrageLab helps portfolio managers and traders who want to leverage the power of statistical arbitrage by providing
+reproducible, interpretable, and easy to use tools.
+"""
+
+import arbitragelab.distance_approach as distance_approach
