300dd292ca7e3fe4adc021e454d24748c7d2691d
PanPip <panpipp@gmail.com>
2020-11-06 20:45:11 +0100

    Added PCA Approach code

---
+++ arbitragelab/__init__.py
@@ -0,0 +1,6 @@
+"""
+ArbitrageLab helps portfolio managers and traders who want to leverage the power of statistical arbitrage by providing
+reproducible, interpretable, and easy to use tools.
+"""
+
+import arbitragelab.other_approaches as other_approaches
---
+++ arbitragelab/other_approaches/__init__.py
@@ -0,0 +1,5 @@
+"""
+This module implements other Statistical Arbitrage strategies
+"""
+
+from arbitragelab.other_approaches.pca_approach import PCAStrategy
