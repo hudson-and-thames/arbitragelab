a2d27be758769fc6fb8b0f8aa73bd979e6734f5e
jackal08 <jacques@quantsportal.com>
2020-11-06 10:53:21 +0000

    Basic setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+# Copyright 2019, Hudson and Thames Quantitative Research
+# All rights reserved
+# Read more: https://github.com/hudson-and-thames/mlfinlab/blob/master/LICENSE.txt
+
+
+# Always prefer setuptools over distutils
+from setuptools import setup
+
+setup()
+
+# Create package
+# python setup.py bdist_wheel
+# python3 -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*  (This is the test repo)
+# twine upload dist/*  (This is official repo)
