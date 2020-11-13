b3e3ae43dcad36361fd74d3b197b580f8ee75555
PanPip <panpipp@gmail.com>
2020-11-13 14:17:29 +0100

    Fix pylint in init ML Approach

--- arbitragelab/__init__.py
+++ arbitragelab/__init__.py
@@ -2,5 +2,6 @@
 ArbitrageLab helps portfolio managers and traders who want to leverage the power of Statistical Arbitrage by providing
 reproducible, interpretable, and easy to use tools.
 """
+
 import arbitragelab.ml_approach as ml_approach
 import arbitragelab.util as util
--- arbitragelab/ml_approach/__init__.py
+++ arbitragelab/ml_approach/__init__.py
@@ -1,8 +1,5 @@
-# Copyright 2019, Hudson and Thames Quantitative Research
-# All rights reserved
-# Read more: https://github.com/hudson-and-thames/mlfinlab/blob/master/LICENSE.txt
 """
-This module houses the ML Based Approaches
+This module houses the ML Based Approaches.
 """
 
 from arbitragelab.ml_approach.pairs_selector import PairsSelector
