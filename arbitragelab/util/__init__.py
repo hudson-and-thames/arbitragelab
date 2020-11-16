83b55f4c5cff21a025f474d049812bd72c2a5122
PanPip <panpipp@gmail.com>
2020-11-16 16:56:00 +0100

    Remove Risk Metrics from init

--- arbitragelab/util/__init__.py
+++ arbitragelab/util/__init__.py
@@ -4,5 +4,4 @@ Utility functions.
 
 from arbitragelab.util.data_importer import DataImporter
 from arbitragelab.util.indexed_highlight import IndexedHighlight
-from arbitragelab.util.risk_metrics import RiskMetrics
 from arbitragelab.util.generate_dataset import get_classification_data
