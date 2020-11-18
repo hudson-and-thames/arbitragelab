fc51356502f7f21f5ee0c09bca12f103cb6d0e54
jackal08 <jacques@quantsportal.com>
2020-11-18 12:22:58 +0000

    hotfix: update links to License.

--- arbitragelab/__init__.py
+++ arbitragelab/__init__.py
@@ -1,3 +1,7 @@
+# Copyright 2019, Hudson and Thames Quantitative Research
+# All rights reserved
+# Read more: https://hudson-and-thames-arbitragelab.readthedocs-hosted.com/en/latest/additional_information/license.html
+
 """
 ArbitrageLab helps portfolio managers and traders who want to leverage the power of Statistical Arbitrage by providing
 reproducible, interpretable, and easy to use tools.
