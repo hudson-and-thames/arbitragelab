e1df565d7e934fb8b0114eca6608482e1bfa2b6a
PanPip <panpipp@gmail.com>
2020-11-20 11:24:51 +0100

    Adjust pylint for main file Copula Strategy

--- arbitragelab/copula_approach/main.py
+++ arbitragelab/copula_approach/main.py
@@ -5,13 +5,17 @@ Notebook for implementing copula strategies.
 """
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+from sklearn.covariance import EmpiricalCovariance
+# from sklearn import preprocessing
+from scipy.stats import norm
+from scipy.stats import t as student_t
+
 import arbitragelab.copula_approach.copula_strategy as copula_strategy
 import arbitragelab.copula_approach.copula_generate as cg
 import arbitragelab.copula_approach.copula_calculation as ccalc
-from sklearn.covariance import EmpiricalCovariance
-from sklearn import preprocessing
-from scipy.stats import norm
+
+# pylint: disable=invalid-name, protected-access
 
 pair_prices = pd.read_csv(r'arbitragelab\BKD_ESC_2009_2011.csv')
 CS = copula_strategy.CopulaStrategy()
@@ -39,7 +43,7 @@ cdf2 = ccalc.find_marginal_cdf(ESC_clr)
 u1 = cdf1(BKD_train)
 u2 = cdf2(ESC_train)
 
-unif_data = np.array([u1,u2]).reshape(2,-1).T 
+unif_data = np.array([u1, u2]).reshape(2, -1).T
 value_data = norm.ppf(unif_data)  # Change from quantile to value.
 # Getting empirical covariance matrix.
 cov_hat = EmpiricalCovariance().fit(value_data).covariance_
@@ -52,7 +56,6 @@ my_copula = Switch.choose_copula(copula_name='Student',
 likelihood_list = [my_copula._c(xi, yi) for (xi, yi) in zip(u1, u2)]
 #logml, copula = ccalc.log_ml(x=u1, y=u2, copula_name='Student', nu=669)
 #%%
-from scipy.stats import t as student_t
 nu = 669
 rho = my_copula.rho
 corr = [[1, rho],
@@ -60,4 +63,3 @@ corr = [[1, rho],
 t_dist = student_t(df=nu)
 y1 = t_dist.ppf(0.5)
 y2 = t_dist.ppf(0.7)
-
