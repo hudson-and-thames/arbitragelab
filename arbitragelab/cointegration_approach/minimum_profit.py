15cf5d6ab79641dc6b4bda2aabaf806dd43c2bd1
Stochastic-Adventure <73219028+Stochastic-Adventure@users.noreply.github.com>
2020-11-24 07:44:58 -0800

    Tqdm fix.

--- arbitragelab/cointegration_approach/minimum_profit.py
+++ arbitragelab/cointegration_approach/minimum_profit.py
@@ -8,13 +8,13 @@ This module optimizes the upper and lower bounds for mean-reversion cointegratio
 and generates the corresponding trading signal.
 """
 
+import sys
 import warnings
 from typing import Optional, Tuple
 
 import numpy as np
 import pandas as pd
 import statsmodels.api as sm
-from tqdm import tqdm
 
 from arbitragelab.cointegration_approach.engle_granger import EngleGrangerPortfolio
 from arbitragelab.cointegration_approach.johansen import JohansenPortfolio
@@ -224,6 +224,33 @@ class MinimumProfit:
         # Return a pandas.Series indexed by grid points for easy retrieval
         passage_time_df = pd.Series(passage_time, index=grid)
         return passage_time_df
+
+    @staticmethod
+    def _print_progress(iteration, max_iterations, prefix='', suffix='', decimals=1, bar_length=50):
+        # pylint: disable=expression-not-assigned
+        """
+        Calls in a loop to create a terminal progress bar.
+        https://gist.github.com/aubricus/f91fb55dc6ba5557fbab06119420dd6a
+        :param iteration: (int) Current iteration.
+        :param max_iterations: (int) Maximum number of iterations.
+        :param prefix: (str) Prefix string.
+        :param suffix: (str) Suffix string.
+        :param decimals: (int) Positive number of decimals in percent completed.
+        :param bar_length: (int) Character length of the bar.
+        """
+        str_format = "{0:." + str(decimals) + "f}"
+        # Calculate the percent completed.
+        percents = str_format.format(100 * (iteration / float(max_iterations)))
+        # Calculate the length of bar.
+        filled_length = int(round(bar_length * iteration / float(max_iterations)))
+        # Fill the bar.
+        block = '█' * filled_length + '-' * (bar_length - filled_length)
+        # Print new line.
+        sys.stdout.write('\r%s |%s| %s%s %s' % (prefix, block, percents, '%', suffix)),
+
+        if iteration == max_iterations:
+            sys.stdout.write('\n')
+        sys.stdout.flush()
 
     def optimize(self, ar_coeff: float, epsilon_t: pd.Series, ar_resid: np.array,
                  horizon: int, granularity: float = 0.01) -> Tuple[float, ...]:
