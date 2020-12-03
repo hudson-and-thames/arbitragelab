76b30e10bd29189fec3d42b2af76821a6e6f0a3c
Stochastic-Adventure <73219028+Stochastic-Adventure@users.noreply.github.com>
2020-12-02 18:35:21 -0800

    Update __init__.py

--- arbitragelab/cointegration_approach/__init__.py
+++ arbitragelab/cointegration_approach/__init__.py
@@ -5,4 +5,3 @@ from arbitragelab.cointegration_approach.johansen import JohansenPortfolio
 from arbitragelab.cointegration_approach.engle_granger import EngleGrangerPortfolio
 from arbitragelab.cointegration_approach.signals import (get_half_life_of_mean_reversion,
                                                          bollinger_bands_trading_strategy, linear_trading_strategy)
-from arbitragelab.cointegration_approach.multi_coint import MultivariateCointegration
