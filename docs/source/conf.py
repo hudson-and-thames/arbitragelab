49aad3351389f3c73f5dea806f8dbb12574c0243
jackal08 <jacques@quantsportal.com>
2020-12-24 12:52:41 +0000

    Bump version: 0.2.1 → 0.2.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Hudson & Thames Quantitative Research.'
 author = 'Hudson & Thames Quantitative Research'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.1'
+release = '0.2.2'
 
 
 # -- General configuration ---------------------------------------------------
