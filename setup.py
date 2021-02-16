37838cd7f2fdaf76e8ffbe72eb607ca84e7b1ea9
PanPip <panpipp@gmail.com>
2021-02-16 13:49:30 +0100

    Updated versions

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ from setuptools import setup
 
 setup()
 
-# Create package
-# python setup.py bdist_wheel
-# twine upload dist/*  (This is official repo)
-
 # Bump version
 # Make sure install codes are updated in the docs.
 # Update Changelog release
 # bumpversion major/minor/patch --allow-dirty
 # git push origin [0.1.0]
+# Double-check if you did a Git Push
 # On Github, go to tags and use the GUI to push a Release.
 
+# Create package
+# python setup.py bdist_wheel
+# twine upload dist/*  (This is official repo)
