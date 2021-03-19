608432b5da90d54299617c9c8bcaf66f4f22e13e
PanPip <panpipp@gmail.com>
2021-03-19 13:51:59 +0100

    Adjusted the PCA Approach file structure

--- arbitragelab/other_approaches/__init__.py
+++ arbitragelab/other_approaches/__init__.py
@@ -2,5 +2,3 @@
 This module implements other Statistical Arbitrage strategies.
 """
 from arbitragelab.other_approaches.kalman_filter import KalmanFilterStrategy
-from arbitragelab.other_approaches.pca_approach import PCAStrategy
-from arbitragelab.other_approaches.etf_approach import ETFStrategy
