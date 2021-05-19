3c7ad0d330215be82e96e03eb953a4377c517c74
PanPip <panpipp@gmail.com>
2021-05-19 18:30:47 +0200

    Adjusted unit tests for Optimal Convergence

--- tests/test_optimal_convergence.py
+++ tests/test_optimal_convergence.py
@@ -53,11 +53,11 @@ class TestOptimalConvergence(unittest.TestCase):
         sc_liu.fit(self.dataframe, mu_m=0.05, sigma_m=0.35, r=0.02)
 
         # Checking parameter values.
-        self.assertAlmostEqual(sc_liu.lambda_1, 0, delta=1e-4)
-        self.assertAlmostEqual(sc_liu.lambda_2, 0, delta=1e-4)
-        self.assertAlmostEqual(sc_liu.b_squared, 0, delta=1e-3)
-        self.assertAlmostEqual(sc_liu.sigma_squared, 0, delta=1e-4)
-        self.assertAlmostEqual(sc_liu.beta, 0, delta=1e-4)
+        self.assertAlmostEqual(sc_liu.lambda_1, -0.009828, delta=1e-4)
+        self.assertAlmostEqual(sc_liu.lambda_2, 0.116263, delta=1e-4)
+        self.assertAlmostEqual(sc_liu.b_squared, 0.083477, delta=1e-3)
+        self.assertAlmostEqual(sc_liu.sigma_squared, -1.034854, delta=1e-4)
+        self.assertAlmostEqual(sc_liu.beta, -2.985708, delta=1e-4)
 
 
     def test_describe(self):
@@ -78,7 +78,7 @@ class TestOptimalConvergence(unittest.TestCase):
                  'lambda_1', 'lambda_2', 'b_squared', 'sigma_squared',
                  'beta']
 
-        data = ['GLD', 'GDX', 0, 0, 0, 0, 0]
+        data = ['GLD', 'GDX', -0.009828, 0.116263, 0.083477, -1.034854, -2.985708]
 
         # Testing the output of describe method
         pd.testing.assert_series_equal(pd.Series(index=index, data=data), sc_liu.describe(), check_exact=False, atol=1e-3)
@@ -105,22 +105,22 @@ class TestOptimalConvergence(unittest.TestCase):
         phi_1, phi_2, phi_m = sc_liu.unconstrained_portfolio_weights_continuous(self.dataframe, gamma=4)
 
         # Checking the values of phi_1 weights
-        self.assertAlmostEqual(np.mean(phi_1), 0, delta=1e-5)
-        self.assertAlmostEqual(phi_1[7],  0, delta=1e-4)
-        self.assertAlmostEqual(phi_1[28], 0, delta=1e-4)
-        self.assertAlmostEqual(phi_1[-1], 0, delta=1e-4)
+        self.assertAlmostEqual(np.mean(phi_1), -0.346020, delta=1e-5)
+        self.assertAlmostEqual(phi_1[7],  -0.359256, delta=1e-4)
+        self.assertAlmostEqual(phi_1[28], -0.347385, delta=1e-4)
+        self.assertAlmostEqual(phi_1[-1], -0.338012, delta=1e-4)
 
         # Checking the values of phi_2 weights
-        self.assertAlmostEqual(np.mean(phi_2), 0, delta=1e-5)
-        self.assertAlmostEqual(phi_2[7],  0, delta=1e-4)
-        self.assertAlmostEqual(phi_2[28], 0, delta=1e-4)
-        self.assertAlmostEqual(phi_2[-1], 0, delta=1e-4)
+        self.assertAlmostEqual(np.mean(phi_2), 0.313540, delta=1e-5)
+        self.assertAlmostEqual(phi_2[7],  0.325810, delta=1e-4)
+        self.assertAlmostEqual(phi_2[28], 0.314856, delta=1e-4)
+        self.assertAlmostEqual(phi_2[-1], 0.305949, delta=1e-4)
 
         # Checking the values of phi_m weights
-        self.assertAlmostEqual(np.mean(phi_m), 0, delta=1e-5)
-        self.assertAlmostEqual(phi_m[7],  0, delta=1e-4)
-        self.assertAlmostEqual(phi_m[28], 0, delta=1e-4)
-        self.assertAlmostEqual(phi_m[-1], 0, delta=1e-4)
+        self.assertAlmostEqual(np.mean(phi_m), 0.005066, delta=1e-5)
+        self.assertAlmostEqual(phi_m[7],  0.002182, delta=1e-4)
+        self.assertAlmostEqual(phi_m[28], 0.004920, delta=1e-4)
+        self.assertAlmostEqual(phi_m[-1], 0.006312, delta=1e-4)
 
 
     def test_delta_neutral_continuous(self):
@@ -144,22 +144,22 @@ class TestOptimalConvergence(unittest.TestCase):
         phi_1, phi_2, phi_m = sc_liu.delta_neutral_portfolio_weights_continuous(self.dataframe, gamma=4)
 
         # Checking the values of phi_1 weights
-        self.assertAlmostEqual(np.mean(phi_1), 0, delta=1e-5)
-        self.assertAlmostEqual(phi_1[7], 0, delta=1e-4)
-        self.assertAlmostEqual(phi_1[28], 0, delta=1e-4)
-        self.assertAlmostEqual(phi_1[-1], 0, delta=1e-4)
+        self.assertAlmostEqual(np.mean(phi_1), -0.330006, delta=1e-5)
+        self.assertAlmostEqual(phi_1[7], -0.342951, delta=1e-4)
+        self.assertAlmostEqual(phi_1[28], -0.331399, delta=1e-4)
+        self.assertAlmostEqual(phi_1[-1], -0.321981, delta=1e-4)
 
         # Checking the values of phi_2 weights
-        self.assertAlmostEqual(np.mean(phi_2), 0, delta=1e-5)
-        self.assertAlmostEqual(phi_2[7], 0, delta=1e-4)
-        self.assertAlmostEqual(phi_2[28], 0, delta=1e-4)
-        self.assertAlmostEqual(phi_2[-1], 0, delta=1e-4)
+        self.assertAlmostEqual(np.mean(phi_2), 0.330006, delta=1e-5)
+        self.assertAlmostEqual(phi_2[7], 0.342951, delta=1e-4)
+        self.assertAlmostEqual(phi_2[28], 0.331399, delta=1e-4)
+        self.assertAlmostEqual(phi_2[-1], 0.321981, delta=1e-4)
 
         # Checking the values of phi_m weights
-        self.assertAlmostEqual(np.mean(phi_m), 0, delta=1e-5)
-        self.assertAlmostEqual(phi_m[7], 0, delta=1e-4)
-        self.assertAlmostEqual(phi_m[28], 0, delta=1e-4)
-        self.assertAlmostEqual(phi_m[-1], 0, delta=1e-4)
+        self.assertAlmostEqual(np.mean(phi_m), 0.102041, delta=1e-5)
+        self.assertAlmostEqual(phi_m[7], 0.102041, delta=1e-4)
+        self.assertAlmostEqual(phi_m[28], 0.102041, delta=1e-4)
+        self.assertAlmostEqual(phi_m[-1], 0.102041, delta=1e-4)
 
 
     def test_wealth_gain_continuous(self):
@@ -183,7 +183,7 @@ class TestOptimalConvergence(unittest.TestCase):
         wealth_gain = sc_liu.wealth_gain_continuous(self.dataframe, gamma=4)
 
         # Checking the values of phi_1 weights
-        self.assertAlmostEqual(np.mean(wealth_gain), 0, delta=1e-5)
-        self.assertAlmostEqual(wealth_gain[7], 0, delta=1e-4)
-        self.assertAlmostEqual(wealth_gain[28], 0, delta=1e-4)
-        self.assertAlmostEqual(wealth_gain[-1], 0, delta=1e-4)
+        self.assertAlmostEqual(np.mean(wealth_gain), 0.999688, delta=1e-5)
+        self.assertAlmostEqual(wealth_gain[7], 0.999411, delta=1e-4)
+        self.assertAlmostEqual(wealth_gain[28], 0.999617, delta=1e-4)
+        self.assertAlmostEqual(wealth_gain[-1], 1.0, delta=1e-4)
