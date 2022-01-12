4de14a07779813e9b6d0b67dd334f890b7a42e82
Oleksandr Proskurin <proskurinolexandr@gmail.com>
2022-01-12 12:39:10 +0200

    Add spread construction function

--- arbitragelab/hedge_ratios/__init__.py
+++ arbitragelab/hedge_ratios/__init__.py
@@ -4,3 +4,4 @@ Module which implements various hedge ratios calculations.
 
 from arbitragelab.hedge_ratios.linear import get_ols_hedge_ratio, get_tls_hedge_ratio
 from arbitragelab.hedge_ratios.half_life import get_minimum_hl_hedge_ratio
+from arbitragelab.hedge_ratios.spread_construction import construct_spread
---
+++ arbitragelab/hedge_ratios/spread_construction.py
@@ -0,0 +1,27 @@
+# Copyright 2019, Hudson and Thames Quantitative Research
+# All rights reserved
+# Read more: https://hudson-and-thames-arbitragelab.readthedocs-hosted.com/en/latest/additional_information/license.html
+"""
+Utility functions used to construct spreads.
+"""
+
+import pandas as pd
+
+
+def construct_spread(price_data: pd.DataFrame, hedge_ratios: pd.Series, dependent_variable: str = None) -> pd.Series:
+    """
+    Construct spread from `price_data` and `hedge_ratios`. If a user sets `dependent_variable` it means that a
+    spread will be: hedge_ratio_dependent_variable * dependent_variable - sum(hedge_ratios * other variables).
+    Otherwise, spread is:  hedge_ratio_0 * variable_0 - sum(hedge ratios * variables[1:]).
+
+    :param price_data: (pd.DataFrame) Asset prices data frame.
+    :param hedge_ratios: (pd.Series) Hedge ratios series (index-tickers, values-hedge ratios).
+    :param dependent_variable: (str) Dependent variable to use. Set None for dependent variable being equal to 0 column.
+    :return: (pd.Series) Spread series.
+    """
+    weighted_prices = price_data * hedge_ratios  # price * hedge
+    if dependent_variable is not None:
+        non_dependent_variables = [x for x in weighted_prices.columns if x != dependent_variable]
+        return weighted_prices[dependent_variable] - weighted_prices[non_dependent_variables].sum(axis=1)
+    else:
+        return weighted_prices[weighted_prices.columns[0]] - weighted_prices[weighted_prices.columns[1:]].sum(axis=1)
