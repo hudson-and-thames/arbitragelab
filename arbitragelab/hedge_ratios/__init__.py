7d4116c6971886487fc22cc0fed3111ea1f132fe
Oleksandr Proskurin <proskurinolexandr@gmail.com>
2022-04-12 11:31:29 +0100

    Add johansen optimal hedge ratio

--- arbitragelab/hedge_ratios/__init__.py
+++ arbitragelab/hedge_ratios/__init__.py
@@ -5,4 +5,5 @@ Module which implements various hedge ratios calculations.
 from arbitragelab.hedge_ratios.linear import get_ols_hedge_ratio, get_tls_hedge_ratio
 from arbitragelab.hedge_ratios.half_life import get_minimum_hl_hedge_ratio
 from arbitragelab.hedge_ratios.adf_optimal import get_adf_optimal_hedge_ratio
+from arbitragelab.hedge_ratios.johansen import get_johansen_hedge_ratio
 from arbitragelab.hedge_ratios.spread_construction import construct_spread
---
+++ arbitragelab/hedge_ratios/johansen.py
@@ -0,0 +1,39 @@
+"""
+Johansen hedge ratio calculation.
+"""
+
+from typing import Tuple
+
+import pandas as pd
+
+from arbitragelab.cointegration_approach import JohansenPortfolio
+from arbitragelab.hedge_ratios.spread_construction import construct_spread
+
+
+def get_johansen_hedge_ratio(price_data: pd.DataFrame, dependent_variable: str) -> Tuple[
+    dict, pd.DataFrame, pd.Series, pd.Series]:
+    """
+    Get hedge ratio from Johansen test eigen vector..
+
+    :param price_data: (pd.DataFrame) DataFrame with security prices.
+    :param dependent_variable: (str) Column name which represents the dependent variable (y).
+    :return: (Tuple) Hedge ratios, X, and y and OLS fit residuals.
+    """
+    port = JohansenPortfolio()
+    port.fit(price_data)
+
+    X = price_data.copy()
+    X.drop(columns=dependent_variable, axis=1, inplace=True)
+
+    y = price_data[dependent_variable].copy()
+    # Convert to a format expected by `construct_spread` function and normalize such that dependent has a hedge ratio 1.
+    hedge_ratios = port.cointegration_vectors.iloc[0].to_dict()
+    for ticker, h in hedge_ratios.items():
+        if ticker != dependent_variable:
+            hedge_ratios[ticker] = -h / hedge_ratios[dependent_variable]
+    hedge_ratios[dependent_variable] = 1.0
+
+    residuals = construct_spread(price_data, hedge_ratios=hedge_ratios, dependent_variable=dependent_variable)
+
+    # Normalize Johansen cointegration vectors such that dependent variable has a hedge ratio of 1.
+    return hedge_ratios, X, y, residuals
