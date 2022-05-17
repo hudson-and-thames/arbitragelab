005d3745bdef6c70669f596f85aea044c37216c0
Oleksandr Proskurin <proskurinolexandr@gmail.com>
2022-05-17 17:30:07 +0100

    Minor changes

--- arbitragelab/spread_trading/z_score.py
+++ arbitragelab/spread_trading/z_score.py
@@ -6,7 +6,6 @@
 Module for signal generation for Bollinger Bands Trading strategy.
 """
 
-import warnings
 from collections import deque
 from statistics import mean, stdev
 from uuid import UUID
@@ -68,19 +67,26 @@ class BollingerBandsTradingRule:
         std_spread = stdev(spread_slice_list[-std_window:])
         return (spread_slice[-1] - mean_spread) / std_spread
 
-    def check_entry_signal(self, spread_series: np.ndarray) -> tuple:
+    def update_spread_value(self, latest_spread_value: float):
         """
-        Function which checks entry condition for a spread series based on z_score_entry.
+        Adds latest spread value to `self.spread_series`. Once it is done - one can check entry/exit signals.
 
-        :param spread_series: (np.ndarray) latest spread series values.
+        :param latest_spread_value: (float) Latest spread value.
+        """
+        self.spread_series.append(latest_spread_value)
+
+    def check_entry_signal(self) -> tuple:
+        """
+        Function which checks entry condition for a spread series based on `self.z_score_entry`, `self.spread_series`.
+
         :return: (tuple) Tuple of boolean entry flag and side (if entry flag is True).
         """
         # Check if std is non-zero, otherwise it will return np.inf which is not tradable.
-        if np.std(spread_series, ddof=1) == 0:
+        if np.std(self.spread_series, ddof=1) == 0:
             return False, None
 
         # Calculate z score
-        z_score = self.get_z_score(spread_series, sma_window=self.sma_window, std_window=self.std_window)
+        z_score = self.get_z_score(self.spread_series, sma_window=self.sma_window, std_window=self.std_window)
 
         # Long entry
         if z_score <= - self.entry_z_score:
@@ -95,7 +101,6 @@ class BollingerBandsTradingRule:
     def add_trade(
             self,
             start_timestamp: pd.Timestamp,
-            label_seria: deque,
             side_prediction: int,
             uuid: UUID = None,
     ):
@@ -103,16 +108,15 @@ class BollingerBandsTradingRule:
         Adds a new trade to track. Calculates trigger prices and trigger timestamp.
 
         :param start_timestamp: (pd.Timestamp) Timestamp of the future label.
-        :param label_seria: (deque) Seria of prices to calculate init z_score from.
         :param side_prediction: (int) External prediction for the future label.
         :param uuid: (str) Unique identifier used to link label to tradelog action.
         """
 
-        initial_z_score = self.get_z_score(label_seria, sma_window=self.sma_window, std_window=self.std_window)
+        initial_z_score = self.get_z_score(self.spread_series, sma_window=self.sma_window, std_window=self.std_window)
         self.open_trades[start_timestamp] = {
             'exit_z_score': initial_z_score + side_prediction * self.exit_z_score_delta,
-            'start_value': label_seria[-1],
-            'label_seria': label_seria,
+            'start_value': self.spread_series[-1],
+            'spread_series': list(self.spread_series),
             'uuid': uuid,
             'side': side_prediction,
             'latest_update_timestamp': start_timestamp,
@@ -130,16 +134,9 @@ class BollingerBandsTradingRule:
 
         formed_trades_uuid = []  # Array of trades formed (uuid)
         to_close = {}  # Trades to close.
+        z_score = self.get_z_score(self.spread_series, self.sma_window, self.std_window)
         for timestamp, data in self.open_trades.items():
-            if update_timestamp == data['latest_update_timestamp']:
-                warnings.warn("Updating event has the same timestamp as the previous one")
-            else:
-                data['latest_update_timestamp'] = update_timestamp
-            if update_timestamp >= data['threshold_timestamp']:
-                to_close[timestamp] = data
-                continue
-            data['label_seria'].append(update_value)
-            z_score = self.get_z_score(data['label_seria'], self.sma_window, self.std_window)
+            data['latest_update_timestamp'] = update_timestamp
             if data['side'] > 0:
                 if z_score >= data['exit_z_score']:
                     to_close[timestamp] = data
