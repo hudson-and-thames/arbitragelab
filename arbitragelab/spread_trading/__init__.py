f89fc5d91d25b022e15720b4d922f6658beee5b8
Illya Barziy <panpipp@gmail.com>
2022-06-14 14:08:47 +0200

    Added MultivariateContegrationTradingRule

--- arbitragelab/spread_trading/__init__.py
+++ arbitragelab/spread_trading/__init__.py
@@ -4,3 +4,4 @@ Class which implements various functions used to trade spreads.
 
 from arbitragelab.spread_trading.z_score import BollingerBandsTradingRule
 from arbitragelab.spread_trading.minimum_profit import MinimumProfitTradingRule
+from arbitragelab.spread_trading.multi_coint import MultivariateCointegrationTradingRule
