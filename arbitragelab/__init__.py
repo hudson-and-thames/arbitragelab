5b4f6ec3a616f90b85281475b3034242bd3f2437
benedettoleto <benny91998@gmail.com>
2024-04-24 18:45:20 +0200

    import mode changed

--- arbitragelab/__init__.py
+++ arbitragelab/__init__.py
@@ -2,18 +2,19 @@
 ArbitrageLab helps portfolio managers and traders who want to leverage the power of Statistical Arbitrage by providing
 reproducible, interpretable, and easy to use tools.
 """
+# pylint: disable=consider-using-from-import
 
-from arbitragelab import codependence
-from arbitragelab import cointegration_approach
-from arbitragelab import copula_approach
-from arbitragelab import distance_approach
-from arbitragelab import hedge_ratios
-from arbitragelab import ml_approach
-from arbitragelab import optimal_mean_reversion
-from arbitragelab import other_approaches
-from arbitragelab import spread_selection
-from arbitragelab import stochastic_control_approach
-from arbitragelab import tearsheet
-from arbitragelab import time_series_approach
-from arbitragelab import trading
-from arbitragelab import util
+import arbitragelab.codependence as codependence
+import arbitragelab.cointegration_approach as cointegration_approach
+import arbitragelab.copula_approach as copula_approach
+import arbitragelab.distance_approach as distance_approach
+import arbitragelab.hedge_ratios as hedge_ratios
+import arbitragelab.ml_approach as ml_approach
+import arbitragelab.optimal_mean_reversion as optimal_mean_reversion
+import arbitragelab.other_approaches as other_approaches
+import arbitragelab.spread_selection as spread_selection
+import arbitragelab.stochastic_control_approach as stochastic_control_approach
+import arbitragelab.tearsheet as tearsheet
+import arbitragelab.time_series_approach as time_series_approach
+import arbitragelab.trading as trading
+import arbitragelab.util as util
